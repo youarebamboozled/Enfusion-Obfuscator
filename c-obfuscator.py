00738cbbe8f465ce69ca6769cb8cef7953024fc2
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2022-08-07 17:02:30 +0200

    added cp and removed not working code

--- c-obfuscator.py
+++ c-obfuscator.py
@@ -1,3 +1,20 @@
+"""
+Enfusion Obfuscator. This Program is used to obfuscate Enfusion style code.
+Copyright (C) 2022  YouAreBamboozled
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import os
 import re
 import math
@@ -8,62 +25,45 @@ import base64
 
 def variable_renamer(given_string):
     """
-    Function to rename all variables and functions.
-    given_string is a string of C/C++ code
+    renames all vars to random names if it works
     """
-    # Variable declarations:
+
     variable_dictionary = {}
     special_cases = {"typedef", "unsigned"}
     index = 0
     new_string = ""
 
-    # Split the code to indicate when it enters/exits a string
     split_code = re.split('\"', given_string)
 
-    # REGEX to find all function and variable declarations ignoring main
     filtered_code = re.findall("(?:\w+\s+)(?!main)^(?!override).*$(?:\*)*([a-zA-Z_][a-zA-Z0-9_]*)", given_string)
 
-    # For loop to add examples found from running a REGEX to a dictionary object
-    # Ignores special cases and repeats
-    # When a value is entered it is also assigned a random string of length 12
     for found_example in filtered_code:
         if found_example not in special_cases:
             if found_example not in variable_dictionary:
                 variable_dictionary[found_example] = random_string(12)
 
-    # For each even section in split code (odd indicates that it is in a string)
-    # replace all the variable and function names with what is defined in the dictionary
     for section in split_code:
         if index % 2 == 0:
             for entry in variable_dictionary:
-                # Used \W because we don't want to replace a variable if it is inside another word.
                 re_string = r"\W{}\W".format(entry)
-                # While loop to go through every entry and replace it
-                # Breaks when it cannot find another instance
                 while True:
                     first_found_entry = re.search(re_string, section)
                     if not first_found_entry:
                         break
-                    # Gets the iterator start and endpoints of the searched re_string
-                    # Then replaces the information inbetween with the dictionary value
                     start = first_found_entry.start(0)
                     end = first_found_entry.end(0)
                     section = section[:start + 1] + variable_dictionary[entry] + section[end - 1:]
-        # Add the current section back to make the original string but with obfuscated names
-        # Accounts for adding a quote everytime except for the first scenario
         if index >= 1:
             new_string = new_string + "\"" + section
         else:
             new_string = new_string + section
         index += 1
-    # Return the obfuscated code
     return new_string
 
 
 def random_string(stringLength=8):
     """
-    Function to generate a random string.
-    Can pass it an integer string length to make it that size else it will be 8
+    Function to generate a random string with given length.
     """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
@@ -78,53 +78,11 @@ def random_comment(stringLength=100):
     return "/*" + ''.join(random.choice(letters) for i in range(stringLength)) + "*/"
 
 
-def random_c_code():
-    c_code = """
-        int {} = {};
-        string {} = "{}";
-    """.format(random_string(12), random_int(0, 100), random_string(12), random_string(12))
-
-    return c_code
-
-
-def random_int(min, max):
+def random_int(mini, maxi):
     """
     Function to generate a random integer between min and max
     """
-    return random.randint(min, max)
-
-
-def whitespace_remover(a):
-    """
-    Function to remove all whitespace, except for after functions, variables, and imports
-    """
-    splits = re.split('\"', a)
-    code_string = "((\w+\s+)[a-zA-Z_*][|a-zA-Z0-9_]*|#.*|return [a-zA-Z0-9_]*| [[.].]|else)"
-    index = 0
-    a = ""
-    for s in splits:
-        # If it's not the contents of a string, remove spaces of everything but code
-        if index % 2 == 0:
-            s_spaceless = re.sub("[\s]", "", s)  # Create a spaceless version of s
-            s_code = re.findall(code_string, s)  # find all spaced code blocks in s
-            for code in s_code:
-                old = re.sub("[\s]", "", code[0])
-                new = code[0]
-                if code[0][0] == '#':
-                    new = code[0] + "\n"  # Adding a newline for preprocessor commands
-                elif "unsigned" in code[0] or "else" in code[0]:
-                    new = code[0] + " "
-                s_spaceless = s_spaceless.replace(old,
-                                                  new)  # Replace the spaceless code blocks in s with their spaced
-                # equivalents
-        else:
-            s_spaceless = s
-        if index >= 1:
-            a = a + "\"" + s_spaceless
-        else:
-            a = a + s_spaceless
-        index += 1
-    return a
+    return random.randint(mini, maxi)
 
 
 def create_typedefs(given_string):
@@ -164,8 +122,7 @@ def create_typedefs(given_string):
 
 def comment_remover(given_string):
     """
-    Function to (currently) remove C++ style comments
-    given_string is a string of C/C++ code
+    Function to remove C style comments
     """
     # Remove all C++ style comments
     cpp_filtered_code = re.findall(r"\/\/.*", given_string)
@@ -181,9 +138,6 @@ def comment_remover(given_string):
 
 
 def main():
-    """
-    The main function to begin the obfuscation of c code files
-    """
     cwd = os.getcwd()
     msg = "5257356d64584e706232346754324a6d64584e6a595852766369424462334235636d6c6e614851674b454d70494449774d6a49674" \
           "9466c76645546795a554a6862574a76623370735a57514b494341674946526f61584d6763484a765a334a686253426a6232316c63" \
@@ -215,7 +169,6 @@ def main():
                 print("PASS\n")
                 file_string = comment_remover(file_string)
                 file_string = variable_renamer(file_string)
-                # file_string = whitespace_remover(file_string)
                 file_string = create_typedefs(file_string)
                 f = open("obfuscated_" + filename, "w+")
                 f.write(file_string)
